a51acf6debfb76090b33a06518983a9d76f6cc48
Jay Fanse <jayf29112003@gmail.com>
2025-02-25 11:49:58 +0530

    UPSc data done

--- scripts/supremeJsonGenerator.py
+++ scripts/supremeJsonGenerator.py
@@ -28,7 +28,7 @@ def generateSupremeJson(supremeData):
     
     
     # central
-    orgs = supremeData["central"]
+    # orgs = supremeData["central"]
 
     # for org in orgs:
     # # for org in orgs[7:]:
@@ -87,76 +87,6 @@ def generateSupremeJson(supremeData):
     #         print("stored in",organization)
 
     # print("Central done")
-
-
-    # state
-    states = supremeData['state']
-
-    for state in states[5:]:
-        stateName = state['name']
-        orgs = state['organizations']
-        
-        if(not os.path.exists(f'../Server/data/Formatted_data/States/{stateName}')):
-            os.mkdir(f'../Server/data/Formatted_data/States/{stateName}')
-        
-        failedState = {
-            "name": stateName,
-            "organizations": []
-        }
-        
-        for org in orgs:
-            organization = org["name"]
-            applyLink = org["applyLink"]
-            links = org["links"]
-
-            file_path = f"../Server/data/Formatted_data/Central/{organization}.json"
-            if(not os.path.exists(file_path)):
-                with open(file_path,'a') as f:
-                    pass
-
-            failedLinks=[]
-            
-            for link in links[:5]:
-                output = generateOutput(link, applyLink)
-                if(output):
-                    
-                    
-
-                    # Step 1: Check if file exists and read existing data
-                    if os.path.exists(file_path):
-                        with open(file_path, "r", encoding="utf-8") as f:
-                            try:
-                                existingEvents = json.load(f)  # Load existing JSON data
-                                if not isinstance(existingEvents, list):  # Ensure it's a list
-                                    existingEvents = []
-                            except json.JSONDecodeError:
-                                existingEvents = []  # If JSON is corrupted or empty, reset it to an empty list
-                    else:
-                        existingEvents = []  # If file doesn't exist, start with an empty list
-
-                    # Step 2: Append new events to the list
-                    existingEvents.append(output)
-
-                    # Step 3: Write back the updated list to the file
-                    with open(file_path, "w", encoding="utf-8") as f:
-                        json.dump(existingEvents, f, ensure_ascii=False, indent=4)
-                        
-                else:
-                    failedLinks.append(link)
-            
-            failedObject = {
-                "name": organization,
-                "applyLink": applyLink,
-                "links": failedLinks
-            }
-            
-            SupremeFailedData["central"].append(failedObject)        
-            with open ('../scripts/SupremeFailedLinksTemp.json','w',encoding='utf-8') as f:
-                json.dump(SupremeFailedData,f)
-            
-            print("stored in",organization)
-
-    print("Central done")
 
 
     # state
@@ -236,72 +166,72 @@ def generateSupremeJson(supremeData):
 
 
     # UPSC
-    # orgs = supremeData["central"]
-    # UPSC = next((org for org in orgs if org["name"] == "UPSC"), None)
+    orgs = supremeData["central"]
+    UPSC = next((org for org in orgs if org["name"] == "UPSC"), None)
 
-    # if(not os.path.exists(f'../Server/data/Formatted_data/Multiple/UPSC')):
-    #     os.mkdir(f'../Server/data/Formatted_data/Multiple/UPSC')
+    if(not os.path.exists(f'../Server/data/Formatted_data/Multiple/UPSC')):
+        os.mkdir(f'../Server/data/Formatted_data/Multiple/UPSC')
         
-    # orgs = UPSC["organizations"]
+    orgs = UPSC["organizations"]
 
-    # failedUPSC = {
-    #     "name":"UPSC",
-    #     "organizations":[]
-    # }
+    failedUPSC = {
+        "name":"UPSC",
+        "organizations":[]
+    }
     
-    # for org in orgs:
-    #     organization = org["name"]
-    #     applyLink = org["applyLink"]
-    #     links = org["links"]
+    for org in orgs:
+        organization = org["name"]
+        applyLink = org["applyLink"]
+        links = org["links"]
 
 
-    #     file_path = f"../Server/data/Formatted_data/Multiple/UPSC/{organization}.json"
-    #     if(not os.path.exists(file_path)):
-    #         with open(file_path,'a') as f:
-    #             pass
+        file_path = f"../Server/data/Formatted_data/Multiple/UPSC/{organization}.json"
+        if(not os.path.exists(file_path)):
+            with open(file_path,'a') as f:
+                pass
 
-    #     failedLinks=[]
-    #     for link in links[:5]:
-    #         output = generateOutput(link, applyLink)
-    #         if(output):
+        failedLinks=[]
+        for link in links[:5]:
+            output = generateOutput(link, applyLink)
+            if(output):
 
-    #             os.makedirs(os.path.dirname(file_path), exist_ok=True)
+                os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-    #             if os.path.exists(file_path):
-    #                 with open(file_path, "r", encoding="utf-8") as f:
-    #                     try:
-    #                         existingEvents = json.load(f)  # Load existing JSON data
-    #                         if not isinstance(existingEvents, list):  # Ensure it's a list
-    #                             existingEvents = []
-    #                     except json.JSONDecodeError:
-    #                         existingEvents = []  # If JSON is corrupted or empty, reset it to an empty list
-    #             else:
-    #                 existingEvents = []  # If file doesn't exist, start with an empty list
+                if os.path.exists(file_path):
+                    with open(file_path, "r", encoding="utf-8") as f:
+                        try:
+                            existingEvents = json.load(f)  # Load existing JSON data
+                            if not isinstance(existingEvents, list):  # Ensure it's a list
+                                existingEvents = []
+                        except json.JSONDecodeError:
+                            existingEvents = []  # If JSON is corrupted or empty, reset it to an empty list
+                else:
+                    existingEvents = []  # If file doesn't exist, start with an empty list
                 
-    #             existingEvents.append(output)
+                existingEvents.append(output)
 
-    #             with open(file_path, "w", encoding="utf-8") as f:
-    #                 json.dump(existingEvents, f, ensure_ascii=False, indent=4)
+                with open(file_path, "w", encoding="utf-8") as f:
+                    json.dump(existingEvents, f, ensure_ascii=False, indent=4)
                 
-    #         else:
-    #             failedLinks.append(link)
+            else:
+                failedLinks.append(link)
             
-    #     failedObject = {
-    #         "name":organization,
-    #         "applyLink": applyLink,
-    #         "links": failedLinks
-    #     }
+        failedObject = {
+            "name":organization,
+            "applyLink": applyLink,
+            "links": failedLinks
+        }
         
-    #     failedUPSC["organizations"].append(failedObject)
+        failedUPSC["organizations"].append(failedObject)
                 
-    #     print("stored in",organization,"json")
+        print("stored in",organization,"json")
 
-    # SupremeFailedData['central'].append(failedUPSC)
+    SupremeFailedData['central'].append(failedUPSC)
     
-    # with open ("../scripts/SupremeFailedLinksTemp.json",'w+', encoding='utf-8') as f:
-    #     json.dump(SupremeFailedData,f) 
+    with open ("../scripts/SupremeFailedLinksTemp.json",'w+', encoding='utf-8') as f:
+        json.dump(SupremeFailedData,f) 
     
-    # print("UPSC done")
+    print("UPSC done")
 
 
     with open ("../scripts/SupremeFailedLinks.json",'w+', encoding='utf-8') as f:
